fcb97470e8bca22ffa76470f0b40f9d4ef69d387
lnh <lnhcode@outlook.com>
2020-11-12 18:35:52 +0800

    Fix pom.xml

--- analysis.bat
+++ analysis.bat
@@ -1,2 +1,2 @@
-SET SONAR_TOKEN=73243c2f3e2108ef877220b48d15be821eb7c50f
+SET SONAR_TOKEN=62f031b88aa1d3ee54aec9bcbf6acd896ecaa991
 mvn clean verify site spotbugs:spotbugs com.github.meixuesong:merge-cpd-pmd-report-maven-plugin:1.0:merge sonar:sonar
